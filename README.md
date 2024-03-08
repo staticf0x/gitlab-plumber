75045fcf30fa594c34ab1c28d6474341d954adfc
staticf0x <staticf0x@pm.me>
2024-03-08 12:17:16 +0100

    Add blame graphs

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# gl-cli
+# gitlab-plumber
+
+GitLab plumber is a small utility that helps you analyze the duration
+of GitLab pipelines.
+
+## Running
+
+### Show a tree of jobs
+
+`$ poetry run python3 main.py show --project <PROJECT_ID> --pipeline <PIPELINE_ID>`
+
+![Pipeline tree](images/show.png)
+
+This shows the queue time + run time for each job as well as percentage of the
+stage duration. For stages, it shows the total runtime and percentage of the
+whole pipeline duration.
+
+### Show a blame graph of stages and individual jobs
+
+`$ poetry run python3 main.py blame --project <PROJECT_ID> --pipeline <PIPELINE_ID>`
+
+![Blame graph](images/blame.png)
+
+This shows the stages and jobs sorted by total run duration (queue is excluded)
+to narrow down which of the stages/jobs take the longest.
 
 ## Installation
 
@@ -12,7 +36,3 @@ Create `.env` file with the following content:
 GITLAB_URI=<GitLab instance URL>
 PRIVATE_TOKEN=<Your private token>
 ```
-
-## Running
-
-- `$ poetry run python3 main.py --project <PROJECT_ID> --pipeline <PIPELINE_ID>`
--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 #!/usr/bin/env python3
 import os
+from dataclasses import dataclass
 
 import arrow
 import click
 import dotenv
 import gitlab
 import rich
+from rich.console import Console
+from rich.progress import BarColumn, Progress, TextColumn
 from rich.status import Status
+from rich.style import Style
 from rich.tree import Tree
+
+
+@dataclass
+class JobDuration:
+    queue: float | None
+    run: float | None
+
+
+@dataclass
+class StageDuration:
+    run: float
+    jobs: dict[str, JobDuration]
+
+    def __lt__(self, other):
+        return self.run < other.run
+
 
 dotenv.load_dotenv()
 gl = gitlab.Gitlab(os.getenv("GITLAB_URI"), private_token=os.getenv("PRIVATE_TOKEN"))
 
 
 def get_stage_tree(stage_name: str, stage_jobs: list, pipeline) -> Tree:
+    """Get a rich.tree.Tree for a given stage, including its jobs."""
     stage_duration = 0.0
     stage_duration_str = ""
 
@@ -78,15 +99,39 @@ def get_stage_tree(stage_name: str, stage_jobs: list, pipeline) -> Tree:
     return tree
 
 
+def get_stage_duration(stage_name: str, stage_jobs: list) -> StageDuration:
+    """Get stage and job durations for a given stage."""
+    stage_duration = 0.0
+
+    if any(job.status != "manual" for job in stage_jobs):
+        if all(job.finished_at is not None for job in stage_jobs if job.status != "manual"):
+            first_job = min(
+                arrow.get(job.started_at) for job in stage_jobs if job.status != "manual"
+            )
+            last_job = max(
+                arrow.get(job.finished_at) for job in stage_jobs if job.status != "manual"
+            )
+
+            delta = last_job - first_job
+            stage_duration = delta.total_seconds()
+
+    job_durations = {}
+
+    for job in stage_jobs:
+        job_durations[job.name] = JobDuration(queue=job.queued_duration, run=job.duration)
+
+    return StageDuration(run=stage_duration, jobs=job_durations)
+
+
 @click.group()
 def cli():
     pass
 
 
-@cli.command(help="Display information about a pipeline")
+@cli.command(help="Show a single pipeline run as a tree of jobs")
 @click.option("--project", "-p", required=True)
 @click.option("--pipeline", required=True)
-def pipeline(project: int, pipeline: int):
+def show(project: int, pipeline: int):
     with Status("Loading pipeline jobs..."):
         project = gl.projects.get(project)
         pipeline = project.pipelines.get(pipeline)
@@ -122,6 +167,89 @@ def pipeline(project: int, pipeline: int):
         print(f"Total: {p_queued:.1f}s + {pipeline.duration:.1f}s = {duration_str}")
     else:
         print("Can't show total time: pipeline hasn't finished yet.")
+
+
+@cli.command(help="Show a blame graph of long running jobs")
+@click.option("--project", "-p", required=True)
+@click.option("--pipeline", required=True)
+def blame(project: int, pipeline: int):
+    with Status("Loading pipeline jobs..."):
+        project = gl.projects.get(project)
+        pipeline = project.pipelines.get(pipeline)
+        jobs = reversed(pipeline.jobs.list(all=True))
+
+    if not pipeline.finished_at:
+        rich.print("[bold red]Cannot analyze running pipelines[/]")
+        return
+
+    console = Console(highlight=False)
+    console.print(
+        f"[b][link={pipeline.web_url}]Pipeline {pipeline.id}[/link][/] in {project.path_with_namespace}\n"
+        f"[grey50]Started at {arrow.get(pipeline.created_at).to('local').format('YYYY-MM-DD HH:mm')} by {pipeline.user['username']}[/]\n"
+    )
+
+    stages = {}
+
+    # Split jobs into stages
+    for job in jobs:
+        stages.setdefault(job.stage, [])
+        stages[job.stage].append(job)
+
+    stage_durations: dict[str, StageDuration] = {}
+
+    for stage, stage_jobs in stages.items():
+        stage_duration = get_stage_duration(stage, stage_jobs)
+        stage_durations[stage] = stage_duration
+
+    total = max(stage_durations.items(), key=lambda v: v[1].run)[1].run
+
+    bar = Progress(
+        TextColumn("{task.description}"),
+        BarColumn(bar_width=50, finished_style="bar.complete"),
+        TextColumn("{task.completed:.1f}s"),
+        TextColumn("({task.fields[perc_time]:.1%})"),
+    )
+
+    # Breakdown by stage
+    for stage, stage_duration in sorted(stage_durations.items(), key=lambda v: v[1], reverse=True):
+        bar.add_task(
+            stage,
+            total=total,
+            completed=stage_duration.run,
+            perc_time=stage_duration.run / pipeline.duration,
+        )
+
+    console.print("[u][b]Breakdown by stage[/]\n")
+    console.print(bar)
+
+    # Breakdown by job for each stage
+    for stage, stage_duration in stage_durations.items():
+        job_durations = [(k, v) for k, v in stage_duration.jobs.items() if v.run is not None]
+
+        bar = Progress(
+            TextColumn("{task.description}"),
+            BarColumn(
+                bar_width=50,
+                finished_style=Style(color="white"),
+                complete_style=Style(color="white"),
+            ),
+            TextColumn("{task.completed:.1f}s"),
+            TextColumn("({task.fields[perc_time]:.1%})"),
+        )
+
+        try:
+            total = max(job_durations, key=lambda v: v[1].run)[1].run
+        except ValueError:
+            # When there are only manual jobs, the job_durations is empty
+            continue
+
+        for job, job_duration in sorted(job_durations, key=lambda v: v[1].run, reverse=True):
+            bar.add_task(
+                job, total=total, completed=job_duration.run, perc_time=job_duration.run / total
+            )
+
+        console.print(f"\n[u]Stage: [b]{stage}[/]\n")
+        console.print(bar)
 
 
 if __name__ == "__main__":
